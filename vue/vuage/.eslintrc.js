697c02b1d9f03767b3d9bf329a44df28777daa7c
mmm25002500 <mmm335202@gmail.com>
2022-10-23 23:28:57 +0800

    Feature: Add vue framework

---
+++ vue/vuage/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/vue3-essential',
+      '@vue/standard'
+    ],
+    parserOptions: {
+      parser: '@babel/eslint-parser'
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'space-before-blocks': 'off',
+      'space-before-function-paren': 'off',
+      'vue/multi-word-component-names': 'off',
+      "comma-dangle": 0,
+      'semi': 0,
+    }
+  }
+  
